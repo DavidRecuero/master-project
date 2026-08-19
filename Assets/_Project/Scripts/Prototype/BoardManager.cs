76f4759655b963779886cbf6da5a2ab9d0697d14
David Recuero <recuero.david@gmail.com>
2026-08-19 11:38:04 +0200

    feat(board): set up and draw pipes

--- Assets/_Project/Scripts/Prototype/BoardManager.cs
+++ Assets/_Project/Scripts/Prototype/BoardManager.cs
@@ -1,5 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class Pipe
+{
+    [Tooltip("Higher layer value will be drawn on top of lower layer pipes")]
+    public int layer;
+
+    [Tooltip("List of positions. Index 0 is the origin, the last index is the destination")]
+    public List<Vector2Int> path;
+}
 
 public class BoardManager : MonoBehaviour
 {
@@ -11,18 +23,11 @@ public class BoardManager : MonoBehaviour
     public int width = 5;
     public int height = 5;
 
+    [Header("Pipes Data")]
+    public List<Pipe> pipes = new List<Pipe>();
+
     [Header("Camera Settings")]
     public float edgeMargin = 1f; // Extra space around the board
-
-
-    private int[,] boardMap = new int[,]
-    {
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0 } 
-    };
 
     void Start()
     {
@@ -35,26 +40,70 @@ public class BoardManager : MonoBehaviour
         //Clear the tilemap before generating the board
         tilemap.ClearAllTiles();
 
+        // --- PRECALCULATE PIPE COLORS ---
+
+        // Sort all pipes from lowest layer to highest layer to precalculate their colors
+        List< Pipe > sortedPipes = pipes.OrderBy(p => p.layer).ToList();
+        Dictionary<Pipe, Color> pipeColors = new Dictionary<Pipe, Color>();
+
+        int count = sortedPipes.Count;
+        for (int i = 0; i < count; i++)
+        {
+            if (count == 1)
+            {
+                // If there's only 1 pipe, it's black
+                pipeColors[sortedPipes[i]] = Color.black;
+            }
+            else
+            {
+                // If there are multiple pipes, interpolate between White (lowest) and Black (highest)
+                // i = 0 (lowest) -> grayscale = 1f (White)
+                // i = count - 1 (highest) -> grayscale = 0f (Black)
+                float grayscale = 1f - ((float)i / (count - 1));
+                pipeColors[sortedPipes[i]] = new Color(grayscale, grayscale, grayscale, 1f);
+            }
+        }
+
+
+        // --- DRAW THE BOARD ---
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                // Get the tile ID from the boardMap array
-                int idTile = boardMap[x, y];
-
                 Vector3Int position = new Vector3Int(x, y, 0);
+                Vector2Int currentPos2D = new Vector2Int(x, y);
 
-                // Draw the tile in the tilemap at the specified position
-                tilemap.SetTile(position, tiles[idTile]);
+                // Draw the base tile
+                tilemap.SetTile(position, tiles[0]);
 
-                // --- DEBUG: RANDOM COLORS ---
+                // Unlock the tile so we can change its color
                 tilemap.SetTileFlags(position, TileFlags.None);
-                Color randomColor = Random.ColorHSV();
-                tilemap.SetColor(position, randomColor);
+
+                // Check if there is a pipe at this exact coordinate
+                Pipe topPipe = GetHighestLayerPipeAt(currentPos2D);
+
+                if (topPipe != null)
+                {
+                    // Paint it with the precalculated grayscale color
+                    tilemap.SetColor(position, pipeColors[topPipe]);
+                }
+                else
+                {
+                    // Background: Fully transparent
+                    tilemap.SetColor(position, Color.clear);
+                }
             }
         }
 
         CenterBoard();
+    }
+
+    // Helper function to find the highest layer pipe at a specific coordinate
+    private Pipe GetHighestLayerPipeAt(Vector2Int pos)
+    {
+        return pipes.Where(p => p.path != null && p.path.Contains(pos))
+                    .OrderByDescending(p => p.layer)
+                    .FirstOrDefault();
     }
 
     // Moves the tilemap to center it in the scene based on its width and height
